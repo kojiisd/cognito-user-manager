76ba0c3aefcdfdd8cbec9fb35d15bcd46aeddfc5
Koji Ishida <kishida58@gmail.com>
2019-12-11 12:13:28 +0900

    escape sequence modification

--- prebuild.sh
+++ prebuild.sh
@@ -2,15 +2,15 @@
 
 # for karate
 hugo new karate/`cat ../now.txt`.md
-echo "{{¥<rawhtml¥>}}" >> content/karate/`cat ../now.txt`.md
+echo "{{\<rawhtml\>}}" >> content/karate/`cat ../now.txt`.md
 echo "`cat ../result_karate.html`" >> content/karate/`cat ../now.txt`.md
-echo "{{¥</rawhtml¥>}}" >> content/karate/`cat ../now.txt`.md
+echo "{{\</rawhtml\>}}" >> content/karate/`cat ../now.txt`.md
 
 # for unittest
 hugo new unittest/`cat ../now.txt`.md
-echo "{{¥<rawhtml¥>}}" >> content/unittest/`cat ../now.txt`.md
+echo "{{\<rawhtml\>}}" >> content/unittest/`cat ../now.txt`.md
 echo "`cat ../result_unittest.html`" >> content/unittest/`cat ../now.txt`.md
-echo "{{¥</rawhtml¥>}}" >> content/unittest/`cat ../now.txt`.md
+echo "{{\</rawhtml\>}}" >> content/unittest/`cat ../now.txt`.md
 
 git --version
 git config --global user.email ${GIT_EMAIL}
